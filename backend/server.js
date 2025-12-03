993a1d7d67b8f3c776b086572e11cf69da074357
Aadarsh <aadarshgautam23gmail.com>
2025-12-03 13:05:44 +0545

    server

--- backend/server.js
+++ backend/server.js
@@ -6,31 +6,9 @@ import notesRoutes from './routes/notes_routes.js';
 
 
 const app= express();
-app.use("./api/notes");
+app.use("./api/notes",notesRoutes);
  
-// app.get("/api/notes",(req,res)=>
-// {
-//     //send the notes 
-//     res.status(200).send("Your got 20 notes");
-// });
 
-// app.post("/api/notes",(req,res)=>
-//     {
-//         //create the notes 
-//     res.status(201).json({message:"Notes created successfllu"});
-//     });
-
-//     app.put("/api/notes/:id",(req,res)=>
-//         {
-//             //create the notes 
-//         res.status(200).json({message:"Notes update successfllu"});
-//         });
-
-//         app.delete("/api/notes/:id",(req,res)=>
-//             {
-//                 //create the notes 
-//             res.status(201).json({message:"Notes deleted successfllu"});
-//             });
 app.listen(5001, ()=>{
     console.log("Server is running on port 5001");
     
