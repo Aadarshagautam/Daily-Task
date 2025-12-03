a6bf8c2541fdf019db0b39ca2a066731e00d5436
Aadarsh <aadarshgautam23gmail.com>
2025-12-03 12:22:44 +0545

    make new folder_routes

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,26 @@ const app= express();
 app.get("/api/notes",(req,res)=>
 {
     //send the notes 
-    res.send("you got 5 notes");
     res.status(200).send("Your got 20 notes");
 });
 
 app.post("/api/notes",(req,res)=>
     {
         //create the notes 
-        res.send("you got 5 notes");
+    res.status(201).json({message:"Notes created successfllu"});
     });
+
+    app.put("/api/notes/:id",(req,res)=>
+        {
+            //create the notes 
+        res.status(200).json({message:"Notes update successfllu"});
+        });
+
+        app.delete("/api/notes/:id",(req,res)=>
+            {
+                //create the notes 
+            res.status(201).json({message:"Notes deleted successfllu"});
+            });
 app.listen(5001, ()=>{
     console.log("Server is running on port 5001");
     
