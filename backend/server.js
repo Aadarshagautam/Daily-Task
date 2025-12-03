c2aeb1901320aea1b0bd6721b805d98aa034daf0
Aadarsh <aadarshgautam23gmail.com>
2025-12-03 12:37:25 +0545

    change app

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,36 @@
 import express from 'express';
+import notesRoutes from './routes/notes_routes.js';
+
+
+
+
 
 const app= express();
+app.use("./api/notes");
  
-app.get("/api/notes",(req,res)=>
-{
-    //send the notes 
-    res.status(200).send("Your got 20 notes");
-});
+// app.get("/api/notes",(req,res)=>
+// {
+//     //send the notes 
+//     res.status(200).send("Your got 20 notes");
+// });
 
-app.post("/api/notes",(req,res)=>
-    {
-        //create the notes 
-    res.status(201).json({message:"Notes created successfllu"});
-    });
+// app.post("/api/notes",(req,res)=>
+//     {
+//         //create the notes 
+//     res.status(201).json({message:"Notes created successfllu"});
+//     });
 
-    app.put("/api/notes/:id",(req,res)=>
-        {
-            //create the notes 
-        res.status(200).json({message:"Notes update successfllu"});
-        });
+//     app.put("/api/notes/:id",(req,res)=>
+//         {
+//             //create the notes 
+//         res.status(200).json({message:"Notes update successfllu"});
+//         });
 
-        app.delete("/api/notes/:id",(req,res)=>
-            {
-                //create the notes 
-            res.status(201).json({message:"Notes deleted successfllu"});
-            });
+//         app.delete("/api/notes/:id",(req,res)=>
+//             {
+//                 //create the notes 
+//             res.status(201).json({message:"Notes deleted successfllu"});
+//             });
 app.listen(5001, ()=>{
     console.log("Server is running on port 5001");
     
