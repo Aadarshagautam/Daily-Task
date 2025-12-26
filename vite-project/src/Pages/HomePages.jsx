5f89b2975df9973c72feae352652aef649105d0b
Aadarshagautam <aadarshgautam23@gmail.com>
2025-12-26 15:17:38 +0545

    change Loads

--- vite-project/src/Pages/HomePages.jsx
+++ vite-project/src/Pages/HomePages.jsx
@@ -39,7 +39,7 @@ const HomePages = () => {
       {isRateLimited && <RateLimitedUI />}
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && (
-          <div className="text-center text-primary py-10"> Loading notes..</div>
+          <div className="text-center text-primary py-10"> Loads notes...</div>
         )}
         {notes.length==0 &&!isRateLimited && <NotesNotFound />}
         {notes.length > 0 && !isRateLimited && (
