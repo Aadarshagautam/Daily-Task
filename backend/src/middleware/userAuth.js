a1d13cbf46915dec989e54efe9fb94481de12864
Aadarshagautam <aadarshgautam23@gmail.com>
2026-01-03 21:40:16 +0545

    userAuth.js working

--- backend/src/middleware/userAuth.js
+++ backend/src/middleware/userAuth.js
@@ -1,7 +1,28 @@
 import jwt from "jsonwebtoken";
 
 
- const userAuth= (req,res,next)=>{
+ const userAuth=async (req,res,next)=>{
+
+const {token}=req.cookies;
+if(!token){
+   return res.json({success:false,message:"Not authorized,no token"});
+
+}
+try{
+  const tokenDecode= jwt.verify(token,process.env.JWT_SECRET);
+  if(tokenDecode.id){
+   req.body.userId=tokenDecode.id;
+
+  }else{
+   return res.json({success:false,message:"Not authorized,token failed"});
+  }
+   next();
+
+
+}catch(error){
+   return res.json({success:false,message:error.message});
+
+}
 
  };
 
