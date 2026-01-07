1cf7087c51a611fde76685fbd6944220b8f8db72
Aadarshagautam <aadarshgautam23@gmail.com>
2026-01-07 15:12:21 +0545

    Login.jsx

--- vite-project/src/Pages/Auth/Login.jsx
+++ vite-project/src/Pages/Auth/Login.jsx
@@ -1,24 +1,43 @@
 import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
+
+  const navigate = useNavigate();
   const [state, setState] = useState('Sign Up');
+  const [name, setName]= useState('');
+  const [email, setEmail]= useState('');
+  const [password, setPassword]= useState('');
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from -blue-200 to-purple-400">
       <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm ">
       <h2 className="text-3xl font-semibold text-white text-center mb-3">{state==='Sign Up'? 'Create Account':'Login'}</h2>
       <p className="text-center text-sm mb-6">{state==='Sign Up'? 'Create your account':'Login to your accounr!'}</p>
       <form>
+        
+        {state==='Sign Up' && (
         <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#33A5C]'>
-          <input className="bg-transparent outline-none" type="text" placeholder="Full Name" required />
+          <input onChange={e => setName(e.target.value)} value={name} className="bg-transparent outline-none" type="text" placeholder="Full Name" required />
+        </div>
+      )}
+        <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#33A5C]'>
+          <input onChange={e => setEmail(e.target.value)} value={email} className="bg-transparent outline-none" type="email" placeholder="Email id" required />
         </div>
         <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#33A5C]'>
-          <input className="bg-transparent outline-none" type="email" placeholder="Email id" required />
+          <input onChange={e => setPassword(e.target.value)} value={password} className="bg-transparent outline-none" type="password" placeholder="Password" required />
         </div>
-        <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#33A5C]'>
-          <input className="bg-transparent outline-none" type="password" placeholder="Password" required />
-        </div>
-        <p className='mb-4 text-indigo-500 cursor-pointer'>Foreget password?</p>
+        <p onClick={()=>navigate("/reset-password")} className='mb-4 text-indigo-500 cursor-pointer'>Foreget password?</p>
+        <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900'>{state}</button>
       </form>
+      // Toggle between Sign Up and Login
+      {state ==='Sign Up'?(<p className='text-gray-400 text-center text-xs mt-4'>Already have an account?{''}
+        <span onClick={()=>setState('Login')} className='text-blue-400 cursor-pointer underline'>Login here</span>
+      </p>):(<p className='text-gray-400 text-center text-xs mt-4'>Don't have an account?{''}
+        <span onClick={()=>setState('Sign Up')} className='text-blue-400 cursor-pointer underline'>Sign Up</span>
+      </p>)}
+      
+      
       </div>
 
     </div>
