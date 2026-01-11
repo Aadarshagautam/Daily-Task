e101573751cbe95c499f40e539ed9411844660d6
Aadarshagautam <aadarshgautam23@gmail.com>
2026-01-11 23:01:32 +0545

    EmailVerifty.jsx

--- vite-project/src/Pages/Auth/EmailVerifty.jsx
+++ vite-project/src/Pages/Auth/EmailVerifty.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const EmailVerifty = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from -blue-200 to-purple-400">EmailVerifty</div>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from -blue-200 to-purple-400">
+      <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+        <h1 className='text-white text-2xl font-semibold text-center mb-4'>Email Verify OTP</h1>
+          <p className='text-center mb-6 text-indigo-300'>
+Enter the 6-digit code sent to your email address to verify your account.
+          </p>
+        
+
+      </form>
+    </div>
   )
 }
 
