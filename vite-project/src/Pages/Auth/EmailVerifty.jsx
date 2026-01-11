f891f8cb95ab13bf70f6ecd4ea818210447e1895
Aadarshagautam <aadarshgautam23@gmail.com>
2026-01-11 23:20:03 +0545

    EmailVerifty.jsx

--- vite-project/src/Pages/Auth/EmailVerifty.jsx
+++ vite-project/src/Pages/Auth/EmailVerifty.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
 const EmailVerifty = () => {
+ const handleInput = (index) =>{
+  if(e.target.value.length>0 && index<inputRef.current.length-1){
+    inputRef.current[index+1].focus()
+  }
+ }
+ const handleKeyDown=(index, e)=>{
+  if(e.key === 'Backspace' && e.target.value===''&& index>0){{
+    inputRef.current[index-1].focus()
+  }
+ }
+ }
+
+  const inputRef = React.useRef([])
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from -blue-200 to-purple-400">
       <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
@@ -10,13 +23,18 @@ Enter the 6-digit code sent to your email address to verify your account.
           </p>
         <div className='flex justify-between mb-8'>
           {Array(6).fill(0).map((_, index)=>{
-            <input type="text" maxLength='1' key={index} required className='w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md'/>
+            <input type="text" maxLength='1' key={index} required className='w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md' 
+            ref={el => inputRef.current[index] = el}
+            onInput={(e)=>handleInput(e.index)}
+            onKeyDown={(e)=>handleKeyDown(e.index)}
+
+            />
           })}
         </div>
-
+<button className='w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full'>Verify email</button>
       </form>
     </div>
   )
 }
 
-export default EmailVerifty
+export default EmailVerifty;
